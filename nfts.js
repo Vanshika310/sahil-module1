abf1bda7d7a64e2c0ccc574e4f5a2d8150fb731d
Vanshika310 <136240773+Vanshika310@users.noreply.github.com>
2023-08-20 15:25:46 +0530

    Add files via upload

---
+++ nfts.js
@@ -0,0 +1,32 @@
+const nfts = [
+    [
+        {
+          "name": "1",
+          "description": "Nature's beauty 1",
+          "image": "https://gateway.pinata.cloud/ipfs/QmVUH34L9USSKF2tR8AQMUhAD4J2RwBhnWD6uHLr2Dk12B/0.png"
+        },
+        {
+          "name": "2",
+          "description": "Nature's beauty 2",
+          "image": "https://gateway.pinata.cloud/ipfs/QmVUH34L9USSKF2tR8AQMUhAD4J2RwBhnWD6uHLr2Dk12B/1.png"
+        },
+        {
+          "name": "3",
+          "description": "Nature's beauty 3",
+          "image": "https://gateway.pinata.cloud/ipfs/QmVUH34L9USSKF2tR8AQMUhAD4J2RwBhnWD6uHLr2Dk12B/2.png"
+        },
+        {
+          "name": "4",
+          "description": "Nature's beauty 4",
+          "image": "https://gateway.pinata.cloud/ipfs/QmVUH34L9USSKF2tR8AQMUhAD4J2RwBhnWD6uHLr2Dk12B/3.png"
+        },
+        {
+          "name": "5",
+          "description": "Nature's beauty 5",
+          "image": "https://gateway.pinata.cloud/ipfs/QmVUH34L9USSKF2tR8AQMUhAD4J2RwBhnWD6uHLr2Dk12B/4.png"
+        }
+      ]
+      
+];
+
+module.exports = nfts;
